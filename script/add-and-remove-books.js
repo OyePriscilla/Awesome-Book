7dcee20c62ae6f5b407c0e8347172a818a8163dc
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-27 14:46:48 +0100

    Create array of objects

--- script/add-and-remove-books.js
+++ script/add-and-remove-books.js
@@ -1,3 +1,14 @@
 const bookTitle = document.querySelector('#book-title');
 const bookAuthor = document.querySelector('#book-author');
 const addBook = document.querySelector('#add-book');
+
+let bookArray = [];
+
+addBook = function(){
+    const bookCollections = {
+        title: bookTitle.value,
+        author: bookAuthor.value
+    }
+}
+
+bookArray.unshift(bookCollections);
