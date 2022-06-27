50ca52cc3c6c098311a7897f8bd95fa22bc628be
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-27 21:39:08 +0100

    Eslint linter check adjustment

--- script/add-and-remove-books.js
+++ script/add-and-remove-books.js
@@ -1,46 +1,39 @@
-const bookTitle = document.querySelector("#book-title");
-const bookAuthor = document.querySelector("#book-author");
-const addBook = document.querySelector("#add-button");
+const bookTitle = document.querySelector('#book-title');
+const bookAuthor = document.querySelector('#book-author');
+const addBook = document.querySelector('#add-button');
 
 const bookArray = [];
 
+// eslint-disable-next-line no-unused-vars
+const remove = function (addBook) {
+  const bookCollections = {
+    title: bookTitle.value,
+    author: bookAuthor.value,
+  };
+  document.querySelector('form').removeChild(addBook.parentNode);
+  bookArray.pop(bookCollections);
+  localStorage.removeItem('Book Title', bookTitle.value);
+  localStorage.removeItem('Book Author', bookAuthor.value);
+};
 function add() {
   return `
-        <input type="text" placeholder="Book Title" id ="book-title" value = ${JSON.stringify(
-          bookTitle.value
-        )}><br><br>
-          <input type="text" placeholder="Book Author" id = "book-author" value = ${JSON.stringify(
-            bookAuthor.value
-          )}><br><br>
+        <input type="text" placeholder="Book Title" id ="book-title" value = ${JSON.stringify(bookTitle.value)}><br><br>
+  <input type="text" placeholder="Book Author" id = "book-author" value = ${JSON.stringify(bookAuthor.value)}><br><br>
           <button onclick = remove(this)>Remove Book</button><hr>
-
     `;
 }
 
 addBook.onclick = function (e) {
-  e.preventDefault(); 
-  let div = document.createElement('div');
+  e.preventDefault();
+  const div = document.createElement('div');
   div.innerHTML = add();
-  document.querySelector("form").prepend(div);
+  document.querySelector('form').prepend(div);
   const bookCollections = {
     title: bookTitle.value,
-    author: bookAuthor.value
+    author: bookAuthor.value,
   };
 
   bookArray.unshift(bookCollections);
-
-  localStorage.setItem("Book Title", bookTitle.value);
-  localStorage.setItem("Book Author", bookAuthor.value);
+  localStorage.setItem('Book Title', bookTitle.value);
+  localStorage.setItem('Book Author', bookAuthor.value);
 };
-
-function remove(addBook) {
-  const bookCollections = {
-    title: bookTitle.value,
-    author: bookAuthor.value
-  };
-  document.querySelector("form").removeChild(addBook.parentNode);
-  bookArrayPop = bookArray.pop(bookCollections);
-
-  localStorage.removeItem("Book Title", bookTitle.value);
-  localStorage.removeItem("Book Author", bookAuthor.value);
-}
