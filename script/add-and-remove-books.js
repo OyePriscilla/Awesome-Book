5fe10d7489d739872efcd00017bc1051aaf3dc7e
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-27 18:33:26 +0100

    Adjust eslint error

--- script/add-and-remove-books.js
+++ script/add-and-remove-books.js
@@ -1,44 +1,46 @@
-const bookTitle = document.querySelector('#book-title');
-const bookAuthor = document.querySelector('#book-author');
-const addBook = document.querySelector('#add-button');
-let bookArray = [];
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const addBook = document.querySelector("#add-button");
 
-addBook.onclick = function(e) {
-    e.preventDefault();
-    console.log('clicked');
-    div = document.createElement('div');
-    div.innerHTML = add();   
-    document.querySelector('form').prepend(div);
-    const bookCollections = {
-        title: bookTitle.value,
-        author: bookAuthor.value
-    }
-    
-    bookArray.unshift(bookCollections);    
+const bookArray = [];
 
-    localStorage.setItem('Book Title', bookTitle.value);
-    localStorage.setItem('Book Author', bookAuthor.value);
-    
-};
-
-function add(){
-    return `
-        <input type="text" placeholder="Book Title" id ="book-title" value = ${JSON.stringify(bookTitle.value)}><br><br>
-          <input type="text" placeholder="Book Author" id = "book-author" value = ${JSON.stringify(bookAuthor.value)}><br><br>
+function add() {
+  return `
+        <input type="text" placeholder="Book Title" id ="book-title" value = ${JSON.stringify(
+          bookTitle.value
+        )}><br><br>
+          <input type="text" placeholder="Book Author" id = "book-author" value = ${JSON.stringify(
+            bookAuthor.value
+          )}><br><br>
           <button onclick = remove(this)>Remove Book</button><hr>
 
-    `;    
+    `;
 }
 
-function remove(addBook){ 
-    const bookCollections = {
-        title: bookTitle.value,
-        author: bookAuthor.value
-    }
-    document.querySelector('form').removeChild(addBook.parentNode);
-     bookArrayPop = bookArray.pop(bookCollections);
-    
-     localStorage.removeItem('Book Title', bookTitle.value);
-     localStorage.removeItem('Book Author', bookAuthor.value);
-    
+addBook.onclick = function (e) {
+  e.preventDefault(); 
+  let div = document.createElement('div');
+  div.innerHTML = add();
+  document.querySelector("form").prepend(div);
+  const bookCollections = {
+    title: bookTitle.value,
+    author: bookAuthor.value
+  };
+
+  bookArray.unshift(bookCollections);
+
+  localStorage.setItem("Book Title", bookTitle.value);
+  localStorage.setItem("Book Author", bookAuthor.value);
+};
+
+function remove(addBook) {
+  const bookCollections = {
+    title: bookTitle.value,
+    author: bookAuthor.value
+  };
+  document.querySelector("form").removeChild(addBook.parentNode);
+  bookArrayPop = bookArray.pop(bookCollections);
+
+  localStorage.removeItem("Book Title", bookTitle.value);
+  localStorage.removeItem("Book Author", bookAuthor.value);
 }
