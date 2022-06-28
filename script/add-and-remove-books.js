9d342a3cc17573d41b1fc143cacb3e90edc460d7
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-28 14:37:42 +0100

    Check for add function syntax error

--- script/add-and-remove-books.js
+++ script/add-and-remove-books.js
@@ -2,43 +2,56 @@ const bookTitle = document.querySelector('#book-title');
 const bookAuthor = document.querySelector('#book-author');
 const addBook = document.querySelector('#add-button');
 
-const bookArray = [];
 
-// eslint-disable-next-line no-unused-vars
-function remove(addBook) {
-  const bookCollections = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-  document.querySelector('.book-form').removeChild(addBook.parentNode);
-  bookArray.pop(bookCollections);
-  localStorage.removeItem('Book Title', bookTitle.value);
-  localStorage.removeItem('Book Author', bookAuthor.value);
-};
-function add() {
-  return `
+const bookArray = [];
+let bookCollections;
+
+// const bookCollections = {
+//   title: bookTitle.value,
+//   author: bookAuthor.value,
+// };
+
+// // eslint-disable-next-line no-unused-vars
+// function remove(addBook) {
+
+// };
+// function add() {
+
+// }
+// addBook.onclick = function (e) {
+//   e.preventDefault();
+
+// };
+
+class Booktitleandauthor {
+  constructor (bookTitle, bookAuthor) {
+    this.bookTitle = bookTitle;
+    this.bookAuthor = bookAuthor;
+  }
+  add() {
+    const div = document.createElement('div');
+    document.querySelector('form').prepend(div);
+    bookArray.unshift(bookCollections);
+    localStorage.setItem('Book Title', bookTitle.value);
+    localStorage.setItem('Book Author', bookAuthor.value);
+    div.innerHTML =
+   `
     <h4>${JSON.stringify(bookTitle.value)} by ${bookAuthor.value} </h4>
     <button onclick = remove(this)>Remove Book</button><hr>
     `;
-}
-addBook.onclick = function (e) {
-  e.preventDefault();
-  const div = document.createElement('div');
-  div.innerHTML = add();
-  document.querySelector('form').prepend(div);
-  const bookCollections = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-
-  bookArray.unshift(bookCollections);
-  localStorage.setItem('Book Title', bookTitle.value);
-  localStorage.setItem('Book Author', bookAuthor.value);
-};
-
-class Booktitleandauthor {
-  constructor (title, author) {
-    this.title = title;
-    this.author = author;
+  }
+  remove(addBook) {
+    document.querySelector('.book-form').removeChild(addBook.parentNode);
+    bookArray.pop(bookCollections);
+    localStorage.removeItem('Book Title', bookTitle.value);
+    localStorage.removeItem('Book Author', bookAuthor.value);
   }
 }
+
+const addBookTA = new Booktitleandauthor (bookTitle.value, bookAuthor.value);
+
+console.log(addBookTA);
+
+addBookTA.add();
+
+addBookTA.remove();
